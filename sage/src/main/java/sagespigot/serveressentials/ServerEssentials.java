c3b70a69d4dd4106485fcba1c269375691c338ef
BuildTools <ijamil.osoria@gmail.com>
2022-03-11 20:57:52 +0100

    set up spigot 1.18 plugin

---
+++ sage/src/main/java/sagespigot/serveressentials/ServerEssentials.java
@@ -0,0 +1,17 @@
+package sagespigot.serveressentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ServerEssentials extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
